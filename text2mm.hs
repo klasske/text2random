9e66f77b34737e6bc7ef01b4a4e33809907f569c
Klaske van Vuurden <kvu022@uit.no>
2015-11-01 22:25:44 +0100

    second

--- text2mm.hs
+++ text2mm.hs
@@ -1,20 +1,39 @@
 import Data.Char (toLower) 
-import Data.Set (toList, fromList)
+import Data.List (sort)
+import System.Random
 
 --remove the . at the end of a sentence
 removeDot :: [Char] -> [Char]
 removeDot [] = []
 removeDot xs 
   | last xs == '.' = init xs
-  | otherwise = xs		
+  | otherwise = xs
+
+--remove the . at the end of a sentence
+removePre :: [Char] -> Char -> [Char]
+removePre [] _ = []
+removePre xs x
+  | head xs == x = tail xs
+  | otherwise = xs
+
+--remove the . at the end of a sentence
+removeSuf :: [Char] -> Char -> [Char]
+removeSuf [] _ = []
+removeSuf xs x
+  | last xs == x = init xs
+  | otherwise = xs
+
+		
 
 --prepare words for the Markov model
-convert w = removeDot (map toLower w)
+--convert w = removeDot (map toLower w)
+convert w = removePre (foldl removeSuf (map toLower w) ".,)!") '('
 
-unique xs = toList $ fromList xs
+--make pairs out of a list
+pair xs = zip xs (tail xs)
 
-getAllWords w pairs = [p2 | (p1, p2) <- pairs, p1 == w]
-getCountPerWordMatrix w pairs = [p2 | (p1, p2) <- pairs, p1 == w]
+--find all unique items in a list
+unique xs = [s1| (s1, s2) <- pair s, s1 /= s2] where s = sort xs
 
 
 countInList _ [] = 0 
@@ -25,14 +44,25 @@ countInList x xs
 -- for each item in a list, find how many 
 getCountPerItem xs = [(x, countInList x xs) | x <- unique xs]
 
-getCountPerWordPairs w pairs = getCountPerItem [p2 | (p1, p2) <- pairs, p1 == w]
+--getCountPerWordPairs w pairs = getCountPerItem [p2 | (p1, p2) <- pairs, p1 == w]
+getCountPerWordPairs w pairs = [p2 | (p1, p2) <- pairs, p1 == w]
+
+formStory :: String -> [(String, [String])] -> [String]
+formStory word lisOfWords = []
+-- get random word from list
+-- find word in frequency list (hashmap?)
+
 
 main = do
   f <- readFile "example"
   -- make a list of all words in lowercase
   let w = map convert ( words [x | x <- f])
   --convert to pairs
-  let pairs = [(x, y)| (x, y) <- zip w (tail w)]
+  let pairs = [(x, y)| (x, y) <- pair w]
   --print pairs
-  -- get each word with the words that follow it in the text
-  mapM_ print [(w', getCountPerWordPairs w' pairs)| w' <- unique w ]
+  -- get each word with the words that follow it in the text 
+  let mapping = [(w', getCountPerWordPairs w' pairs)| w' <- unique w ]
+  --now starting at a random word (let's say "a"), we could start printing the word with the highest likelihood. then find that 
+  --word, and get the most likely word following that one
+  n <- getStdRandom (randomR (0, (length mapping) - 1))
+  print (mapping !! n)
